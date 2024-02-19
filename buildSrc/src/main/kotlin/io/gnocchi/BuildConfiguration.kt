f3f0465d702200ec838fb2cd119398320088bbc7
Jonas Rosendo <jonasrosendo1@gmail.com>
2024-02-19 20:55:38 -0300

    changing version code

--- buildSrc/src/main/kotlin/io/gnocchi/BuildConfiguration.kt
+++ buildSrc/src/main/kotlin/io/gnocchi/BuildConfiguration.kt
@@ -4,8 +4,8 @@ object BuildConfiguration {
     const val compileSdk = 34
     const val targetSdk = 34
     const val minSdk = 21
-    const val versionCode = 2
-    const val versionName = "0.1.2"
+    const val versionCode = 3
+    const val versionName = "0.1.3"
     const val groupId = "io.gnocchi"
     const val artifactId = "gnocchi"
 }
